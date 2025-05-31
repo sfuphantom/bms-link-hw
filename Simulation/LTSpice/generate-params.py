38e9f1d5e884784abcc20c20a42e672cc02c78b7
Dan. Renardson <drenardson868@gmail.com>
2025-05-31 12:15:07 -0700

    Initial wokring simulation

--- Simulation/LTSpice/generate-params.py
+++ Simulation/LTSpice/generate-params.py
@@ -3,7 +3,7 @@ import random
 count = 20
 
 ## Thermistor
-BThermNom = 3380   # 
+BThermNom = 3380   # Nominal termistor B value
 BThermTol = 0.01   # 1% Tolerance on thermistor
 R0ThermNom = 10000 # Nominal 10k resistance at 25C
 R0ThermTol = 0.01  # % Tolerance on thermistor resistance at 25C
@@ -16,14 +16,17 @@ RFixedTol = 0.01 # % Tolerance on fixed resistor value
 lines = []
 for i in range(1, count + 1):
 
-    # Create .param lines for x20 B values with tolerance
-    lines.append(f".param BTherm{i} = {BThermNom}*(1+2*{BThermTol}*({random.random():.6f} - 0.5))")
+    # Create .param lines for x20 B values with tolerance, append actual value in LTSpice comment
+    rv = random.random() # Regenerate random variable for each param
+    lines.append(f".param BTherm{i} = {BThermNom}*(1+2*{BThermTol}*({rv:.6f} - 0.5)) ; Actual BTherm{i} = {BThermNom*(1+2*BThermTol*(rv - 0.5))}")
 
     # .params for thermistor resistance at 25C with tolerance applied
-    lines.append(f".param R0Therm{i} = {R0ThermNom}*(1+2*{R0ThermTol}*({random.random():.6f} - 0.5))")
+    rv = random.random() # Regenerate random variable for each param
+    lines.append(f".param R0Therm{i} = {R0ThermNom}*(1+2*{R0ThermTol}*({rv:.6f} - 0.5)) ; Actual R0Therm{i} = {R0ThermNom*(1+2*R0ThermTol*(rv - 0.5))}")
 
     # .params for thermistor resistance at 25C with tolerance applied
-    lines.append(f".param RFixed{i} = {RFixedNom}*(1+2*{RFixedTol}*({random.random():.6f} - 0.5))")
+    rv = random.random() # Regenerate random variable for each param
+    lines.append(f".param RFixed{i} = {RFixedNom}*(1+2*{RFixedTol}*({rv:.6f} - 0.5)) ; Actual RFixed{i} = {RFixedNom*(1+2*RFixedTol*(rv - 0.5))}")
 
 
 
