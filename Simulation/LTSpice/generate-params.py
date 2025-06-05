3f335e385fdfe861703e12084278c1a3f39ea377
Dan. Renardson <drenardson868@gmail.com>
2025-06-05 13:26:50 -0700

    moved all params to python script, added 100k thermistor option

--- Simulation/LTSpice/generate-params.py
+++ Simulation/LTSpice/generate-params.py
@@ -3,18 +3,53 @@ import random
 # There are 20 temperature sensors in the circuit
 count = 20
 
-## Thermistor
-BThermNom = 3380   # Nominal termistor B value
-BThermTol = 0.01   # 1% Tolerance on thermistor
-R0ThermNom = 10000 # Nominal 10k resistance at 25C
-R0ThermTol = 0.01  # % Tolerance on thermistor resistance at 25C
+# lines list stores all the text that we will put in the params.txt file
+lines = []
 
-## Fixed Resistor
-RFixedNom = 1000 # Nominal fixed resistor value 
+# Use this to choose which thermistor part number to apply to the simulation
+# 1 : 10k@25C B=3380K MF52C1103F3380
+# 2 : 100k@25C B=4150 MF52C1104F4150
+thermSelect = 1
+
+# Params obtained from:
+# https://www.digikey.ca/en/products/detail/cantherm/MF52C1103F3380/1840604
+if thermSelect == 1:
+    BThermNom = 3380   # Nominal termistor B value
+    BThermTol = 0.01   # 1% Tolerance on thermistor
+    R0ThermNom = 10000 # Nominal 10k resistance at 25C
+    R0ThermTol = 0.01  # % Tolerance on thermistor resistance at 25C
+
+# Params obtained from: 
+# https://www.digikey.ca/en/products/detail/cantherm/mf52c1104f4150/1840605
+if thermSelect == 2:
+    BThermNom = 4150   # Nominal termistor B value
+    BThermTol = 0.01   # 1% Tolerance on thermistor
+    R0ThermNom = 100000 # Nominal 10k resistance at 25C
+    R0ThermTol = 0.01  # % Tolerance on thermistor resistance at 25C
+
+# Add params from selected thermistor to the list
+lines.append(f".param BThermNom = {BThermNom}")
+lines.append(f".param BThermTol = {BThermTol}")
+lines.append(f".param R0ThermNom = {R0ThermNom}")
+lines.append(f".param R0ThermTol = {R0ThermTol}")
+lines.append(f".param T0Therm = 25+273.15") # R0 of thermisor always measured at T = 25C = 298.19K
+
+# Common node pulldown resistor 
+RPdnTol = 0.01
+RPdnNom = 100*10**3 # 100k, ** is equal to ^ power operator
+lines.append(f".param RPdnTol = {RPdnTol}")
+lines.append(f".param RPdnNom = {RPdnNom}")
+rv = random.random()
+lines.append(f".param Rpdn = {RPdnNom}*(1+2*{RPdnTol}*({rv:.6f} - 0.5)) ;  Actual RPdn = {RPdnNom*(1+2*RPdnTol*(rv - 0.5))}")
+
+# Fixed Resistor 
 RFixedTol = 0.01 # % Tolerance on fixed resistor value
+RFixedNom = 1*10**3 # Nominal fixed resistor value
+lines.append(f".param RFixedTol = {RFixedTol}")
+lines.append(f".param RFixedNom = {RFixedNom}")
+
 
 # Generate the parameter lines
-lines = []
 for i in range(1, count + 1):
 
     # Create .param lines for x20 B values with tolerance, append actual value in LTSpice comment
